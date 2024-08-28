81881ce925aa68acfdc5c417f09eeee3db32f7b0
LAPTOP-ET8FN48H\joann <joannakrol942@gmail.com>
2024-08-28 16:34:27 +0200

    Add new viewModels and bindings

---
+++ MacronutrientsMonitor/ViewModels/DishViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MacronutrientsMonitor.ViewModels
+{
+    internal class DishViewModel : BaseViewModel
+    {
+        private string _dishName;
+        private decimal _protein;
+        private decimal _fat;
+        private decimal _carbohydrates;
+        private int _calories;
+
+        public string DishName
+        {
+            get
+            {
+                return _dishName;
+            }
+            set
+            {
+                _dishName = value;
+                OnPropertyChanged(nameof(DishName));
+            }
+        }
+
+        public decimal Protein
+        {
+            get
+            {
+                return _protein;
+            }
+            set
+            {
+                _protein = value;
+                OnPropertyChanged(nameof(Protein));
+            }
+        }
+
+        public decimal Fat
+        {
+            get
+            {
+                return _fat;
+            }
+            set
+            {
+                _fat = value;
+                OnPropertyChanged(nameof(Fat));
+            }
+        }
+
+        public decimal Carbohydrates
+        {
+            get
+            {
+                return _carbohydrates;
+            }
+            set
+            {
+                _carbohydrates = value;
+                OnPropertyChanged(nameof(Carbohydrates));
+            }
+        }
+
+        public int Calories
+        {
+            get
+            {
+                return _calories;
+            }
+            set
+            {
+                _calories = value;
+                OnPropertyChanged(nameof(Calories));
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        public ICommand CancelCommand { get; }
+
+        public DishViewModel()
+        {
+                
+        }
+    }
+}
---
+++ MacronutrientsMonitor/ViewModels/MakeRecipeViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents.Serialization;
+using System.Windows.Input;
+
+namespace MacronutrientsMonitor.ViewModels
+{
+    public class MakeRecipeViewModel : BaseViewModel
+    {
+        private string _recipeName;
+        private decimal _protein;
+        private decimal _fat;
+        private decimal _carbohydrates;
+        private int _calories;
+        public string RecipeName 
+        { 
+            get 
+            { 
+                return _recipeName; 
+            } 
+            set
+            {
+                _recipeName = value;
+                OnPropertyChanged(nameof(RecipeName));
+            }
+        }
+
+        public decimal Protein
+        {
+            get
+            {
+                return _protein;
+            }
+            set
+            {
+                _protein = value;
+                OnPropertyChanged(nameof(Protein));
+            }
+        }
+
+        public decimal Fat
+        {
+            get
+            {
+                return _fat;
+            }
+            set
+            {
+                _fat = value;
+                OnPropertyChanged(nameof(Fat));
+            }
+        }
+
+        public decimal Carbohydrates
+        {
+            get
+            {
+                return _carbohydrates;
+            }
+            set
+            {
+                _carbohydrates = value;
+                OnPropertyChanged(nameof(Carbohydrates));
+            }
+        }
+
+        public int Calories
+        {
+            get
+            {
+                return _calories;
+            }
+            set
+            {
+                _calories = value;
+                OnPropertyChanged(nameof(Calories));
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        public ICommand CancelCommand { get; }
+
+        public MakeRecipeViewModel()
+        {
+                
+        }
+    }
+}
