cbc1ef6469fc3d24990004c7a769921a253d1c4c
LAPTOP-ET8FN48H\joann <joannakrol942@gmail.com>
2024-09-01 20:34:18 +0200

    Create base dependency injection configuration

--- MacronutrientsMonitor/App.xaml.cs
+++ MacronutrientsMonitor/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using MacronutrientsMonitor.ViewModels;
+﻿using MacronutrientsMonitor.Models;
+using MacronutrientsMonitor.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Configuration;
 using System.Data;
 using System.Windows;
@@ -10,15 +13,34 @@ namespace MacronutrientsMonitor
     /// </summary>
     public partial class App : Application
     {
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder().ConfigureServices(services =>
+            {
+                services.AddSingleton<NutrientsMonitor>();
+                services.AddSingleton<MainWindowViewModel>();
+                services.AddSingleton(s => new MainWindow()
+                {
+                    DataContext = s.GetRequiredService<MainWindowViewModel>()
+                });
+            }).Build();   
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
-            MainWindow = new MainWindow()
-            {
-                DataContext = new MainWindowViewModel()
-            };
+            _host.Start();
+            MainWindow = _host.Services.GetRequiredService<MainWindow>();
             MainWindow.Show();
 
             base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _host.Dispose();
+            base.OnExit(e);
         }
     }
 
